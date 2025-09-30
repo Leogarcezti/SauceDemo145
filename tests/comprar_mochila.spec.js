ad7905b973a150605edc436878ea0d6682da6d7f
Leogarcezti <garcez.leonardo@gmail.com>
2025-09-29 22:11:37 -0300

    Criação de pastas com organização por data e hora

--- tests/comprar_mochila.spec.js
+++ tests/comprar_mochila.spec.js
@@ -1,56 +1,57 @@
-//1 - Referência e bibliotecas
-//Declara um objeto chamado test vindo da biblioteca playwright/test
-const { test, expect} = require('@playwright/test');
+// 1 - Referência e bibliotecas
+// Declara um objeto chamado test vindo da biblioteca Playwrigth
+const { test, expect } = require('@playwright/test')
 
-//2- Classes ou Funções ou Metódos
-// Um script pode executar de forma: - Sincrona ou Assíncrona
-
-
-test('Realizar o fluxo de compra da mochila', async ({ page })=> {
-
-    await page.goto('https://www.saucedemo.com') // Acessa o site
-    await expect(page).toHaveURL('https://www.saucedemo.com') // Verifica se está na página raiz
-    const botao_login = page.locator('#login-button') // Identifica o botão Login
-    await expect(botao_login).toHaveText('Login') // Verifica se o botão Login está visível
-
-    // Realizar o login 
-    await page.fill('[name="user-name"]', 'standard_user') // Preenche o campo usuário
-    await page.fill('[placeholder="Password"]', 'secret_sauce') // Preenche o campo senha 
-    await botao_login.click(); // Clica no botão Login
+// 2 - Classe ou Funções ou Métódos
+// Um script pode executar de forma:
+// - Sincrona: Simultâneo. Ex.: ligação de voz
+// - Assincrona: Separados. Ex.: mensagem de texto no WhatsApp
+test('Realizar o fluxo de compra da mochila', async ({page}) => {
+  
+    await page.goto('/') // abre o browser no site alvo
+    await expect(page).toHaveURL('/')            // verifica se está na página raiz
+    const botao_login = page.locator('#login-button')
+    await expect(botao_login).toHaveText('Login') // verifica elemento escrito Login
+    
+    // Página Inicial Realizar o login
+    // Preencher o campo cujo localizador é name com o valor standard_user
+    await page.fill('[name="user-name"]', 'standard_user')
+    // Preencher o campo cujo localizador é cssSelector com o valor secret_sauce
+    await page.fill('[placeholder="Password"]', 'secret_sauce')
+    // Clicar no botão Login
+    await botao_login.click()
 
     // Página de Inventário / Produtos
-    // Verificar se o usuário está na página de inventário
-    await expect(page).toHaveURL(/.*inventory/) // Verifica se está na página de inventário
-    const tituloSecao= 'span.title' // Identifica o título da seção
-    await expect(page.locator(tituloSecao)).toHaveText('Products')// Verifica se o título da página é Products
+    // Verificar se está na página certa
+    await expect(page).toHaveURL(/.*inventory/)
+    // Se precisar mudar de valor 2 ou mais vezes
+    // Trocar de const (constante) para let (variável)
+    const tituloSecao = '.title' //cssSelector
+    await expect(page.locator(tituloSecao)).toHaveText('Products') 
 
     // Adicionar a mochila ao carrinho de compras
-    const btnAdicionar = '[data-test="add-to-cart-sauce-labs-backpack"]'
-    await page.locator(btnAdicionar).click() 
+    const btnAdicionar = 'xpath=/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button'
+    await page.locator(btnAdicionar).click()
 
+    // Verificar se exibe o nº 1 no carrinho de compras
+    const icoQuantCart = 'span.shopping_cart_badge' //cssSelector
+    // const icoQuantCart = '#shopping_cart_container > a' //Selector
+    await expect(page.locator(icoQuantCart)).toHaveText('1')
 
+    // Clicar no icone do carrinho (nº 1)
+    await page.locator(icoQuantCart).click()
+    
+    // Verificar se está na página certa - Cart
+    await expect(page).toHaveURL(/.*cart/)
+    // tituloSecao permanece igual ao da página Inventory
+    await expect(page.locator(tituloSecao)).toHaveText('Your Cart') 
 
+    // Verificar dados funcionais
+    await expect(page.locator('.cart_quantity')).toHaveText('1')
+    await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack')
+    await expect(page.locator('.inventory_item_price')).toHaveText('$29.99')
 
-    //Verificar se o carrinho de compras está com 1 item
-    const icoQuantidadeCart= 'span.shopping_cart_badge' // Identifica o ícone de quantidade do carrinho por css selector
-    await expect(page.locator(icoQuantidadeCart)).toHaveText('1') // Verifica se o carrinho está com 1 item
+    // Espera de 1 segundo
+    await page.waitForTimeout(1000) // mal visto // alfinete // temporária
 
-
-    // Clicar no ícone do carrinho de compras
-    await page.locator(icoQuantidadeCart).click() // Clica no ícone do carrinho de compras
-
-    //Espera de 3 segundos
-    await page.waitForTimeout(3000) //Mal visto no mercado
-
-
-
-
-
-
-
-
-
-
-});// final do teste
-
-//3 - Ações ou Comandos ou Interações ou Validações
+}) // final do teste
