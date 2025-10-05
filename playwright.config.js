4c068f121c250db50b790a1e64f21db20ab65b2c
Leogarcezti <garcez.leonardo@gmail.com>
2025-10-05 12:16:55 -0300

    Inclusão de funções para capturas de prints nos testes

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,44 @@
-const { defineConfig } = require('@playwright/test')
+const { defineConfig, devices } = require('@playwright/test')
+const path = require('path')
+const { compute_run_folder, ensure_subdirs } = require('./utils/path_tools')
+
+// Diretórios onde ficam os artefatos
+const ARTIFACTS_ROOT = path.join(__dirname, 'artifacts') //nome da pasta raiz
+const rundDir = compute_run_folder(ARTIFACTS_ROOT)
+const {resultsDir, screenshotsDir} = ensure_subdirs(rundDir)
+
+// Exões caminhos de diretórios como vairiáveis de ambiente
+process.env.RUN_DIR         = rundDir
+process.env.SCREENSHOTS_DIR = screenshotsDir
 
 module.exports = defineConfig({
+    testDir: './tests', // nossos testes estão na pasta testes  
+    timeout: 30000, // cada teste pode durar no máximo 30 segundos
+    fullyParallel: true, // testes podem ser executados em paralelo / multi thread
+    outputDir: resultsDir, 
     use: {
         baseURL: 'https://www.saucedemo.com',
         headless: false, // false - exibe o browser e true oculta
+        // Políticas globais de artefatos automaticos
+        screenshot: 'only-on-failure', // apenas quando der erro
+        video: 'retain-on-failure',   // apenas quando der erro
+        trace: 'retain-on-failure',   // apenas quando der erro
+        
+        // outros tipos de timeout
+        actionTimeout: 15000, // timeout se nada estiver acontecendo em 15 segundos
+        navigationTimeout: 20000, // cada navegação pode durar no máximo 20 segundos
+        
         launchOptions: {
-            slowMo: 500    // espere 1 segundo entre cada ação
+            slowMo: 0    // espere 1 segundo entre cada ação
         }
-    }
-})
+    },
+
+   projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome']}
+
+        }    
+    ]
+        
+     })        
